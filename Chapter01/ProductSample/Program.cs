a2f738d347be57eb8607e66e5ac1c68b23f5eeae
infosys <infosys@C4214>
2025-04-21 09:58:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+
+            Product daihuku = new Product(323, "大福",380);
+
+
+            Console.WriteLine( daihuku.Name+ "の平均価格はだいたい" +daihuku.Code + "円らしい");
+            
+
+
+
+            //税抜き価格の表示
+
+            Console.WriteLine("税抜き価格:"+ karinto.Code);
+
+
+            //消費税額の表示
+            Console.WriteLine("消費税額:"+ karinto.GetTax());
+
+
+
+            //税込み価格
+            Console.WriteLine("税込み:"+ karinto.Price);
+
+            
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Diagnostics.Metrics;
+
+namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                FeetToMeter(1, 10);
+            } else {
+                MeterToFeet(1, 10);
+
+            }
+        }
+
+        private static void MeterToFeet(int v1, int v2) {
+            throw new NotImplementedException();
+        }
+
+        static void FeetToMeter(int start,int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+        }
+
+        static void MeterToFeet(int start,int stop){
+            for (int meter = start; meter <= stop meter++) ;
+            double feet = MeterToFeet(meter);
+            Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+
+            
+            }
+        
+        }
+
+    }           
+

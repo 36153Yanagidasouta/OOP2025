b2f004a77f346072e981141f68d6ca945cad3c51
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-05-09 14:56:46 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -31,5 +31,35 @@ namespace SalesCalculator{
             }
             return dict;
         }
+
+               
+
+        //売り上げデータ読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+
+            //売り上げデーターを入れるリストオブジェクトを生成
+            List<Sale> sales = new List<Sale>();
+
+            //ファイルを一気に読み込み
+            string[] lines = File.ReadAllLines(filePath);
+
+            //一回ずつ文字列を持ってくる
+            foreach (string line in lines) {
+
+
+                string[] items = line.Split(',');
+
+                //Saleオブジェクトを生成
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
     }
 }

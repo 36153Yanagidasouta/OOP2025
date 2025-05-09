b8f8d6d7daf05756a9ff25f0de3f061f857a09fd
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-05-09 13:54:26 +0900

    売上集計プログラム初版完成

---
+++ Chapter02/SalesCalculator/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator
+{
+   public class Sale
+        // 売り上げクラス
+    {
+        //店舗名
+        public string ShopName { get; set; } =String.Empty;
+
+
+
+
+        //商品カテゴリ
+        public string ProductCategory { get; set; } = String.Empty;
+
+
+
+        //売上高
+        public int Amount { get; set; }
+
+        
+    }
+}

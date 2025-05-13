58d30fa0381464622598d3283cebe87432bfcf48
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-05-13 11:30:17 +0900

    2-1の4完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,27 +1,33 @@
-﻿namespace Exercise01 {
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercise01 {
     public class Program {
         static void Main(string[] args) {
 
             //2-1の3
-    var songs = new Song[] {
-
-
-    new Song("Let it be", "The Beatles", 243),
-    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-    new Song("Close To You", "Carpenters", 276),
-    new Song("Honesty", "Billy Joel", 231),
-    new Song("I Will Always Love You", "Whitney Houston", 273),
-
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
             };
-}
+            printSongs(songs);
+        }
 
 
         //2-1の4
-        private static void printSongs(Song[] songs) {
+        public static void printSongs(Song[] songs) {
+
+            foreach (var song in songs) {
+
+                var minutes = song.Lenght / 60;
+                var second = song.Lenght % 60;
 
 
+                Console.WriteLine($"{song.Title},{song.ArtistName},{minutes} : {second:00}");
 
+            }
         }
-
     }
 }

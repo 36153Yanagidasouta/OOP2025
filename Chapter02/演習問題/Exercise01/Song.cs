10837de95e1cbd4a76a042226c4a22af2484c736
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-05-13 10:24:36 +0900

    2-1の1改訂と完成

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -4,18 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01{
+namespace Exercise01 {
     //2-1の1
-   public class Song{
+    public class Song {
 
-        String Title;
+        public string Title { get; set; } = String.Empty;
 
-        String ArtistName;
+        public string ArtistName { get; set; } = String.Empty;
 
-        int Length;
+        public int Lenght { get; set; }
 
+
+    //2-1の2
+        public Song(String Title, String ArtistName, int Lenght) {
+
+
+
+
+
+        }
     }
-
-    
-
 }

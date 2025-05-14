660661c4325596bd3fa0d36f87916a5926170181
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-05-14 10:52:45 +0900

    0514演習 出力できずに未完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -5,25 +5,48 @@ namespace Exercise01 {
         static void Main(string[] args) {
 
             //2-1の3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-            printSongs(songs);
+
+            var songs = new List<Song>();
+
+            Console.WriteLine("***曲の登録***");
+
+            while (true) {
+
+                Console.WriteLine("曲名");
+                string title = Console.ReadLine();
+
+                if (title.Equals("end")) return;
+
+                Console.WriteLine("作者名");
+                string artistname = Console.ReadLine();
+
+                Console.WriteLine("演奏時間(秒)");
+                int lenght = int.Parse(Console.ReadLine());
+
+                var songer = new Song(title, artistname, lenght);
+
+               /* Song songer =new Song();
+                title = title;
+                artistname = artistname;
+                lenght = lenght;*/
+
+                songs.Add(songer);
+
+                Console.WriteLine();
+
+            }
+
+            printSongs();
         }
 
 
         //2-1の4
-        public static void printSongs(Song[] songs) {
+        public static void printSongs(songs) {
 
             foreach (var song in songs) {
 
                 var minutes = song.Lenght / 60;
                 var second = song.Lenght % 60;
-
 
                 Console.WriteLine($"{song.Title},{song.ArtistName},{minutes} : {second:00}");
 
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -8,18 +8,21 @@ namespace Exercise01 {
     //2-1の1
     public class Song {
 
-        public string Title { get; private set; } = String.Empty;
-        public string ArtistName { get; private set; } = String.Empty;
-        public int Lenght { get; private set; }
+        public string Title { get;  set; } = String.Empty;
+        public string ArtistName { get;  set; } = String.Empty;
+        public int Lenght { get;  set; }
 
 
     //2-1の2
         public Song(String Title, String ArtistName, int Lenght) {
 
-            this.Title = Title;
+           this.Title = Title;
             this.ArtistName = ArtistName;
             this.Lenght = Lenght;
 
+        }
+
+        public Song() {
         }
     }
 }

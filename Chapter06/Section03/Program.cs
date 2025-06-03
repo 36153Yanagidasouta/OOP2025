0a8e6878d417a686c1f4f718139565d28ca04a4a
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-03 12:08:59 +0900

    Chapter06　演習問題完成

---
+++ Chapter06/Section03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Section03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var languages = new[] { "C#", "Java", "Python", "Ruby", };
+
+
+
+            var sb = new StringBuilder();
+            foreach (var word in GetWord()) {
+                sb.Append(word);
+            }
+            var text = sb.ToString();
+            Console.WriteLine(text);
+
+            string str = "";
+            foreach(var word in GetWord()) {
+                str += word;
+            }
+            Console.WriteLine(str);
+        }
+
+        private static IEnumerable<object> GetWord() {
+            return ["Orange", "Lemon", "Straberry"];
+
+
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=柴塚健斗;BestWork=伊藤伝記;Born=2005";
+
+            foreach (var pair in line.Split(';')) {
+                var word = pair.Split('=');
+                Console.WriteLine($" {ToJapanese(word[0])}:{word[1]}");
+
+            }
+
+            /// </summary>
+            /// <param name="key">"Novelist","BestWork","Born"</param>
+            /// <returns>"「作家」,「代表作」,「誕生年」</returns>
+            static string ToJapanese(string key) {
+
+                return key switch {
+                    "Novelist" => "作家",
+                    "BestWork" => "代表作",
+                    "Born" => "誕生年",
+                    _ => "引数keyは、正しい値ではありません"
+                };
+
+
+                return ""; //エラーをなくすためのダミー
+            }
+        }
+    }
+}
+

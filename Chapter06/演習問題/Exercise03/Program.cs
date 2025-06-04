be74def14029ccba4b3cf5b56fbfda96170cabe0
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-04 11:27:43 +0900

    文字数別カウント、できなかった。

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -1,12 +1,12 @@
 ﻿
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
-
-            Console.WriteLine("6.3.1");
+            #region            Console.WriteLine("6.3.1");
             Exercise1(text);
 
             Console.WriteLine("6.3.2");
@@ -20,6 +20,51 @@ namespace Exercise03 {
 
             Console.WriteLine("6.3.5");
             Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise6(string text) {
+
+            /*List<char> WordsList = new List<char>();
+              List<int> AnserList = new List<int>();
+
+              foreach (var c in text.Order().ToLower()) {
+                  WordsList.Add(c);
+                  if (WordsList[c] == WordsList[c - 1]) {
+                      AnserList[c] = 1;
+                  } else {
+                      AnserList[c - 1] += 1;
+                  }
+                  Console.WriteLine(AnserList);
+              }*/
+
+            var str = text.ToLower().Replace(" ","");
+            var alphaDicCount = Enumerable.Range('a', 26)
+                                .ToDictionary(num => ((char)num).ToString(), num => 0);
+             foreach(var alpha in str) {
+                alphaDicCount[alpha.ToString()]++;
+            }                                    
+            
+             foreach(var item in alphaDicCount) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+
+            }
+
+            /*   ----------------     */
+            var array = Enumerable.Repeat(0, 26).ToArray();
+
+            foreach (var alph in str) {
+                array[alph - 'a']++;
+            }
+
+            for(char ch = 'a';ch <= 'z';ch++ ) {
+                Console.WriteLine($"{ch}:{array[ch -'a']}");
+            }
+
+
         }
 
         private static void Exercise1(string text) {
@@ -44,8 +89,6 @@ namespace Exercise03 {
             var anser = sb.ToString().TrimEnd();
             Console.WriteLine(anser + ".");
         }
-
-
 
         private static void Exercise4(string text) {
             var count = text.Split(' ').Length;

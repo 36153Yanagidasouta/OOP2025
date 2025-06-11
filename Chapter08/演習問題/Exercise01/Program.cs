8f7280305c3c21f81d18c95db02190d7489da9f3
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-11 11:21:23 +0900

    8.1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+            Console.WriteLine();
+        }
+
+        private static void Exercise1(string text) {
+
+            var textDict = new Dictionary<char, int>();
+
+            foreach (var n in text.ToUpper().OrderBy(n => n)) {
+                if ('A' <= n && n <= 'Z') {
+                    if (textDict.ContainsKey(n)) {
+                        textDict[n]++;
+                    } else {
+                        textDict[n] = 1;
+                    }
+                }
+                foreach (var (key, value) in textDict) {
+                    Console.WriteLine($"'{key}':'{value}'");
+                }
+            }
+        }
+
+        private static void Exercise2(string text) {
+            var textDict = new Dictionary<char, int>();
+
+            foreach (var n in text.ToUpper()) {
+                if ('A' <= n && n <= 'Z') {
+                    if (textDict.ContainsKey(n)) {
+                        textDict[n]++;
+                    } else {
+                        textDict[n] = 1;
+                    }
+                }
+                foreach (var (key, value) in textDict) {
+                    Console.WriteLine($"'{key}':'{value}'");
+
+                }
+            }
+        }
+    }
+}

a830eb517172c40e75bdf92fa618221726cb3be7
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-17 10:32:57 +0900

    演習問題8完成

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -11,6 +11,18 @@
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+
+            if (!abbrs.Remove("NPT")) {
+
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
+
+
+
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {

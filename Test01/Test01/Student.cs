4d9d6fde4e2a6d70193603a98ce5cc34ad40a681
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-17 11:10:21 +0900

    設問1_Student クラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+
+        private string Name { get; set; } = String.Empty; //学生の名前
+        
+        private string Subject { get; set; } = String.Empty;　//科目名
+        
+        private int Score { get; set; }　//点数
+
+    }
+}

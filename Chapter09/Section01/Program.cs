04c4a288c190c09789787877fbca17583fba4607
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-06-27 10:01:25 +0900

    P214 問題9.1完成

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Common;
+using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            /*     var today =new  DateTime(2025,7,12);　//日付を返す
+                   var now = DateTime.Now;　　　//時間と日付
+
+                   Console.WriteLine($"Today:{today.Month}");
+                   Console.WriteLine($"Today:{now}");*/
+
+
+            //①自分の生年月日は何曜日かを書いて調べる
+
+            Console.WriteLine("生年月日を入力");
+            Console.Write("西暦:");
+            var year = int.Parse(Console.ReadLine());
+
+            Console.Write( "月:" );
+            var month = int.Parse(Console.ReadLine());
+
+            Console.Write("日");
+            var day = int.Parse(Console.ReadLine());
+
+            var birthday = new DateTime(year, month, day);
+
+
+
+            var culuter = new CultureInfo("ja-JP");
+            culuter.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = birthday.ToString("ggyy年M月d日", culuter);
+
+
+            var dayofweek = culuter.DateTimeFormat.GetShortestDayName(birthday.DayOfWeek);
+            Console.Write(str + dayofweek+ "曜日");
+
+
+            //②うるう年の判定プログラムを作成
+
+            var isLeapYear = DateTime.IsLeapYear(birthday.Year);
+            if (isLeapYear) {
+                Console.WriteLine("うるう年です");
+            }
+            Console.WriteLine("うるう年ではない");
+
+
+            //③生まれてから何日?
+
+            var date1 = birthday;
+            var date2 = DateTime.Today;
+
+            TimeSpan diff = date2 - date1;
+
+            Console.WriteLine($"生まれてから{diff.Days}日");
+
+            
+            //④あなたは○○歳です
+
+            Console.WriteLine($"年齢{diff.Days / 365}歳");
+
+
+
+            //⑤一月一日から何日目か
+            var today = DateTime.Today;
+            int dayOfYear = today.DayOfYear;
+            Console.WriteLine(dayOfYear);
+
+
+        }
+    }
+}
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var dateTime = DateTime.Now;
+
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+
+            var today = DateTime.Now;
+            var s1 = dateTime.ToString("d");
+
+            Console.WriteLine(s1);
+        }
+
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+
+            var s2 = dateTime.ToString("yyyy年MM月dd日HH時mm分ss秒 ");
+
+            Console.WriteLine(s2);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+
+            var today = DateTime.Now;
+
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = today.ToString("ggyy年M月d日", culture);
+            var dayofweek = culture.DateTimeFormat.GetDayName(today.DayOfWeek);
+
+            Console.WriteLine($"{str}({dayofweek})");
+        }
+    }
+}

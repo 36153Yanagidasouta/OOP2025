51002d5f23aff190fa01bdc7b2bfc5edbc5e771d
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-10-29 09:51:01 +0900

    課題③途中

---
+++ Chapter13/Exercise01/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+
+            Exercise1_2();
+
+
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+
+            var book = Library.Books
+                        .MaxBy(b => b.Price);
+            Console.WriteLine(book);
+
+        }
+
+        private static void Exercise1_3() {
+
+            var groups = Library.Books
+            .GroupBy(b => b.PublishedYear)
+            .GroupBy(g => g.Count())
+            .OrderBy(b => b.Key);   
+            foreach (var item in groups) {
+                foreach (var book in item) {
+                    Console.WriteLine($"  {book.Key}年:{book.Count()}");
+                }   
+            }
+        }
+
+
+
+        private static void Exercise1_4() {
+
+        }
+
+        private static void Exercise1_5() {
+
+        }
+
+        private static void Exercise1_6() {
+
+        }
+
+        private static void Exercise1_7() {
+
+        }
+
+        private static void Exercise1_8() {
+
+        }
+    }
+}

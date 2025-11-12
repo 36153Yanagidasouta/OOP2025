934cd93aeb99586953c7bd207513dc6422cfba1f
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-11-12 11:47:15 +0900

    344完成

---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using TextFileProcessor;
+
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            TextFileProcessor.TextProcessor.Run<LineCounterProcessor>(args[0]);
+
+
+        }
+    }
+}

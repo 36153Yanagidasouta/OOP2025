d0df0d42f8460d76983fbdb7865f21cab9e1e42f
Yanagidasouta <oibc36153@ojs.ac.jp>
2025-11-17 15:05:26 +0900

    依存性の注入対応

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/TextFileProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    class TextFileProcessor {
+        private ITextFileService _service;
+
+        public TextFileProcessor(ITextFileService service) {
+            _service = service;
+        }
+
+        public void  Run(string fname) {
+            _service.Initalize(fname);
+            
+            var lines = File.ReadAllLines(fname);
+            foreach (var line in lines) {
+                _service.Execute(line);
+            }
+            _service.Terminate();
+        }
+    }
+}
